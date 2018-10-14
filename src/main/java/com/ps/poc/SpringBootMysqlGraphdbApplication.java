659744577bc40ac9705e1a4a092d4b0baf0cc141
PremRohan <PremRohan@DESKTOP-SFICS3L>
2018-10-14 19:24:02 +0530

    initial SpringBoot Application

---
+++ src/main/java/com/ps/poc/SpringBootMysqlGraphdbApplication.java
@@ -0,0 +1,12 @@
+package com.ps.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlGraphdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlGraphdbApplication.class, args);
+	}
+}
